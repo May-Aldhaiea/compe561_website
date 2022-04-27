fcd04db1e882f3a20975ce853abe38361bdfa8ec
JohnSadiq <Johnsadiq55@gmail.com>
2022-04-26 22:28:11 -0700

    basic connection

--- templates/__init__.py
+++ templates/__init__.py
@@ -1,21 +1,33 @@
-from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+import pymysql
+import secrets
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
+conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 
-def create_app():
+
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "decapitatedSquirrelButt4201337"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(app)
+    app.config['SQLALCHEMY_DATABASE_URI'] = conn
+    db = SQLAlchemy(app)
 
-    from .views import views
-    from .auth import auth
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
 
-    create_database(app)
+    class usernameandpassworddemo(db.Users):
+        id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.String(255))
+        password = db.Column(db.String(255))
+
+    class SignupForm(FlaskForm):
+        username = StringField('Username:', validators=[DataRequired()])
+        password = StringField('Password:', validators=[DataRequired()])
+
+    @app.route('/add_User', method['GET', 'PUSH'])
+    def add_User():
+        form = SignupForm()
+        if form.validate_on_submit():
+            userInfo = usernameandpassworddemo(username=form.username.data, password=form.password.data)
+            db.session.add(userInfo)
+            db.session.commit()
 
 
     return app
---
+++ templates/secrets.py
@@ -0,0 +1,4 @@
+dbuser = 'root'
+dbpass = 'rootiPad.77-4'
+dbhost = '127.0.0.1'
+dbname = 'database.db'
